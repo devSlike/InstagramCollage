31bcd80b439a99b6d913eaf2bbf5a4d4817865fc
devSlike <aloha-bit@mail.ru>
2015-09-30 14:24:25 +0300

    Add empty project InstagramCollage

---
+++ InstagramCollage/InstagramCollage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InstagramCollage.MvcApplication" Language="C#" %>
---
+++ InstagramCollage/InstagramCollage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InstagramCollage.Startup))]
+namespace InstagramCollage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
